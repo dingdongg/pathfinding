9aae648f8efa3e05578da0e1f789f17c3fa86474
donggyu <donggyu258@gmail.com>
2022-05-26 23:51:11 -0700

    add additional class attributes

--- src/Pathfinding/Bfs.ts
+++ src/Pathfinding/Bfs.ts
@@ -4,6 +4,12 @@ import {Pathfinder, PathInfo, SearchedNode} from "./Pathfinder";
 export class BFS implements Pathfinder {
     BOARD_HEIGHT: number;
     BOARD_WIDTH: number;
+    searchOrder: SearchedNode[] = [];
+    shortestPath: any;
+    pathFound: any;
+    grid: any;
+    startNode: any;
+    endNode: any;
 
     constructor(width: number, height: number) {
         this.BOARD_HEIGHT = height;
