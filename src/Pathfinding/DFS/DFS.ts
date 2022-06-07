ba3b50276a32a9e4d12f2763755ad68dff60e38c
donggyu <donggyu258@gmail.com>
2022-06-06 17:48:41 -0700

    create properties and designed constructor

--- src/Pathfinding/DFS/DFS.ts
+++ src/Pathfinding/DFS/DFS.ts
@@ -2,9 +2,27 @@ import { NodeType } from "../../App/Node";
 import {IGrid, INode, Pathfinder, PathInfo, SearchedNode} from "../Pathfinder";
 
 export class DFS implements Pathfinder {
+    grid: IGrid;
+    startNode: INode | undefined;
+    endNode: INode | undefined;
+    stack: INode[];
+
+    searchOrder: SearchedNode[];
+    shortestPath: number[];
+    pathFound: boolean;
     
     constructor(height: number, width: number, nodes: INode[]) {
-        
+        this.grid = {
+            height, width, nodes
+        }
+
+        this.startNode = undefined;
+        this.endNode = undefined;
+        this.stack = [];
+
+        this.searchOrder = [];
+        this.shortestPath = [];
+        this.pathFound = false;
     }
 
 
