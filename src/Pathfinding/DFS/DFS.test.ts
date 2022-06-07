e611ca295616477093b72e750c74d229092c386d
donggyu <donggyu258@gmail.com>
2022-06-06 19:49:09 -0700

    Create disjoint graph test + one more common case

--- src/Pathfinding/DFS/DFS.test.ts
+++ src/Pathfinding/DFS/DFS.test.ts
@@ -19,3 +19,31 @@ test('DFS1', () => {
     expect(result.searchOrder.map(node => node.distance)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
     expect(result.shortestPath).toEqual([16, 9, 2, 3, 4, 5, 6, 13, 20]);
 });
+
+test("DFS2", () => {
+    let testGrid: IGrid = Util.createGrid(4, 7);
+    testGrid.nodes[16].nodeType = NodeType.StartNode;
+    Util.setWallList(testGrid, [4, 11, 18, 19]);
+    testGrid.nodes[20].nodeType = NodeType.EndNode;
+
+    let algorithm: Pathfinder = new DFS(testGrid.height, testGrid.width, testGrid.nodes);
+    const result: PathInfo = algorithm.findPath();
+    expect(result.pathFound).toEqual(true);
+    expect(result.searchOrder.map(node => node.index)).toEqual([16, 9, 2, 3, 10, 17, 24, 25, 26, 27, 20]);
+    expect(result.searchOrder.map(node => node.distance)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(result.shortestPath).toEqual([16, 9, 2, 3, 10, 17, 24, 25, 26, 27, 20]);
+});
+
+test("DFS3", () => {
+    let testGrid: IGrid = Util.createGrid(4, 7);
+    testGrid.nodes[16].nodeType = NodeType.StartNode;
+    Util.setWallList(testGrid, [4, 11, 18, 25]);
+    testGrid.nodes[20].nodeType = NodeType.EndNode;
+
+    let algorithm: Pathfinder = new DFS(testGrid.height, testGrid.width, testGrid.nodes);
+    const result: PathInfo = algorithm.findPath();
+    expect(result.pathFound).toEqual(false);
+    expect(result.searchOrder.map(node => node.index)).toEqual([16, 9, 2, 3, 10, 17, 24, 23, 22, 15, 8, 1, 0, 7, 14, 21]);
+    expect(result.searchOrder.map(node => node.distance)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+    expect(result.shortestPath).toEqual([]);
+});
